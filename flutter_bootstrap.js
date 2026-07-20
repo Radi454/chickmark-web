af60f35cabc9db9eb16902b0ceee770603cdf5c0
El Cheex <ibrahim.radi454@gmail.com>
2026-07-21 00:37:55 +0300

    deploy: publish latest ChickMark main

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3925856473" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2797290400" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
